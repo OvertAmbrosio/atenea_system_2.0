8724ed786cdc1751e21c2d47a63c580e8167bb43
Your NameOvert <zero_uno@hotmail.com>
2020-05-28 05:36:52 -0500

    28-05
    Signed-off-by: Your NameOvert <zero_uno@hotmail.com>

--- src/lib/obtenerFiltros.ts
+++ src/lib/obtenerFiltros.ts
@@ -2,6 +2,88 @@ import { IOrden } from '../models/Orden';
 
 export default async function obtenerFiltros (tipo:string|any, data:Array<IOrden>) {
   switch (tipo) {
+    case 'averiashfc':
+      return new Promise(async(resolve, reject) => {
+        let contratas:Array<string>|any = [];
+        let estados:Array<string>|any = [];
+        let segmentos:Array<string>|any = [];
+        let distritos:Array<string>|any = [];
+        let zonal:Array<string>|any = [];
+        let nodo:Array<string>|any = [];
+        
+        let filtroContrata:Array<Object> = [];
+        let filtroEstado:Array<Object> = [];
+        let filtroSegmento:Array<Object> = [];
+        let filtroDistrito:Array<Object> = [];
+        let filtroZonal:Array<Object> = [];
+        let filtroNodo:Array<Object> = [];
+  
+        await data.map((d) => {
+          if(!contratas.includes(d.contrata_asignada?.nombre_contrata)) {
+            contratas.push(d.contrata_asignada?.nombre_contrata);
+            filtroContrata.push({text: d.contrata_asignada?.nombre_contrata, value: d.contrata_asignada?.nombre_contrata});
+          }
+          if(!estados.includes(d.contrata_asignada?.estado)) {
+            estados.push(d.contrata_asignada?.estado);
+            filtroEstado.push({text: d.contrata_asignada?.estado, value: d.contrata_asignada?.estado});
+          }
+          if(!segmentos.includes(d.codigo_segmento)) {
+            segmentos.push(d.codigo_segmento);
+            filtroSegmento.push({text: d.codigo_segmento, value: d.codigo_segmento});
+          }
+          if(!distritos.includes(d.distrito)) {
+            distritos.push(d.distrito);
+            filtroDistrito.push({text: d.distrito, value: d.distrito});
+          }
+          if (!zonal.includes(d.hfc_detalle.codigo_zonal)) {
+            zonal.push(d.hfc_detalle.codigo_zonal);
+            filtroZonal.push({text: d.hfc_detalle.codigo_zonal, value: d.hfc_detalle.codigo_zonal});
+          }
+          if (!nodo.includes(d.hfc_detalle.codigo_nodo)) {
+            nodo.push(d.hfc_detalle.codigo_nodo);
+            filtroNodo.push({text: d.hfc_detalle.codigo_nodo, value: d.hfc_detalle.codigo_nodo});
+          }
+        });
+        resolve({filtroContrata, filtroEstado, filtroSegmento, filtroDistrito, filtroZonal, filtroNodo})
+      });
+    case 'altashfc':
+      return new Promise(async(resolve, reject) => {
+        let contratas:Array<string>|any = [];
+        let estados:Array<string>|any = [];
+        let segmentos:Array<string>|any = [];
+        let distritos:Array<string>|any = [];
+        let nodo:Array<string>|any = [];
+        
+        let filtroContrata:Array<Object> = [];
+        let filtroEstado:Array<Object> = [];
+        let filtroSegmento:Array<Object> = [];
+        let filtroDistrito:Array<Object> = [];
+        let filtroNodo:Array<Object> = [];
+  
+        await data.map((d) => {
+          if(!contratas.includes(d.contrata_asignada?.nombre_contrata)) {
+            contratas.push(d.contrata_asignada?.nombre_contrata);
+            filtroContrata.push({text: d.contrata_asignada?.nombre_contrata, value: d.contrata_asignada?.nombre_contrata});
+          }
+          if(!estados.includes(d.contrata_asignada?.estado)) {
+            estados.push(d.contrata_asignada?.estado);
+            filtroEstado.push({text: d.contrata_asignada?.estado, value: d.contrata_asignada?.estado});
+          }
+          if(!segmentos.includes(d.codigo_segmento)) {
+            segmentos.push(d.codigo_segmento);
+            filtroSegmento.push({text: d.codigo_segmento, value: d.codigo_segmento});
+          }
+          if(!distritos.includes(d.distrito)) {
+            distritos.push(d.distrito);
+            filtroDistrito.push({text: d.distrito, value: d.distrito});
+          }
+          if (!nodo.includes(d.hfc_detalle.codigo_nodo)) {
+            nodo.push(d.hfc_detalle.codigo_nodo);
+            filtroNodo.push({text: d.hfc_detalle.codigo_nodo, value: d.hfc_detalle.codigo_nodo});
+          }
+        });
+        resolve({filtroContrata, filtroEstado, filtroSegmento, filtroDistrito, filtroNodo})
+      });
     case 'basicas':
       return new Promise(async(resolve, reject) => {
         let contratas:Array<string>|any = [];
